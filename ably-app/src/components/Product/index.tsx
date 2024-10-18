7e0ec47a93ad67fb9592128d49901b5a388f87ea
yuri295 <jin_astro0302@naver.com>
2024-10-18 21:58:33 +0900

    feat(Image): 이미지 컴포넌트 추가

--- ably-app/src/components/Product/index.tsx
+++ ably-app/src/components/Product/index.tsx
@@ -1,7 +1,12 @@
-import {ProductInfo} from '@/components/Product/ProductInfo';
+import { Stack } from '@mui/material';
+import { ProductInfo } from '@/components/Product/ProductInfo';
+import { Image } from '@/components/Image';
 
 export const Product = () => {
-    return (
-        <ProductInfo/>
-    );
+  return (
+    <Stack>
+      <Image alt='상품 이미지 준비중' src='images/product1.webp' />
+      <ProductInfo />
+    </Stack>
+  );
 };
